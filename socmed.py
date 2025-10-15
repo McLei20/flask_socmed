7e2d5e2474d2877fe22dfd8806070194bbe57567
McLei20 <mcf886064@gmail.com>
2025-10-15 21:30:16 +0800

    inputs

---
+++ socmed.py
@@ -0,0 +1,16 @@
+from flask import *
+
+app = Flask(__name__)
+
+@app.route('/login')
+def login():
+    return render_template('create.html')
+@app.route('/output', methods=['POST', 'GET'])
+def output():
+    data = request.form
+    return render_template('output.html', output=data)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
