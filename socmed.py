02fd8a7110ae9fab67d0a0745d2f53bf9678d424
McLei20 <mcf886064@gmail.com>
2025-10-15 21:48:39 +0800

    final

--- socmed.py
+++ socmed.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 @app.route('/login')
 def login():
     return render_template('create.html')
+
 @app.route('/output', methods=['POST', 'GET'])
 def output():
     data = request.form
